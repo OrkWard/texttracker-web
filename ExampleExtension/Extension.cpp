2a8345f3a807109d1454152ed4b7583bd183d531
OrkWard <orkward_o@outlook.com>
2024-02-28 16:53:05 +0800

    style: format

--- ExampleExtension/Extension.cpp
+++ ExampleExtension/Extension.cpp
@@ -1,52 +1,58 @@
 #include "Extension.h"
 
-BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH:
-		MessageBoxW(NULL, L"Extension Loaded", L"Example", MB_OK);
-		break;
-	case DLL_PROCESS_DETACH:
-		MessageBoxW(NULL, L"Extension Removed", L"Example", MB_OK);
-		break;
-	}
-	return TRUE;
+BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call,
+                    LPVOID lpReserved) {
+  switch (ul_reason_for_call) {
+  case DLL_PROCESS_ATTACH:
+    MessageBoxW(NULL, L"Extension Loaded", L"Example", MB_OK);
+    break;
+  case DLL_PROCESS_DETACH:
+    MessageBoxW(NULL, L"Extension Removed", L"Example", MB_OK);
+    break;
+  }
+  return TRUE;
 }
 
-//#define COPY_CLIPBOARD
-//#define EXTRA_NEWLINES
+// #define COPY_CLIPBOARD
+// #define EXTRA_NEWLINES
 
 /*
-	Param sentence: sentence received by Textractor (UTF-16). Can be modified, Textractor will receive this modification only if true is returned.
-	Param sentenceInfo: contains miscellaneous info about the sentence (see README).
-	Return value: whether the sentence was modified.
-	Textractor will display the sentence after all extensions have had a chance to process and/or modify it.
-	The sentence will be destroyed if it is empty or if you call Skip().
-	This function may be run concurrently with itself: please make sure it's thread safe.
-	It will not be run concurrently with DllMain.
+        Param sentence: sentence received by Textractor (UTF-16). Can be
+   modified, Textractor will receive this modification only if true is returned.
+        Param sentenceInfo: contains miscellaneous info about the sentence (see
+   README). Return value: whether the sentence was modified. Textractor will
+   display the sentence after all extensions have had a chance to process and/or
+   modify it. The sentence will be destroyed if it is empty or if you call
+   Skip(). This function may be run concurrently with itself: please make sure
+   it's thread safe. It will not be run concurrently with DllMain.
 */
-bool ProcessSentence(std::wstring& sentence, SentenceInfo sentenceInfo)
-{
-	// Your code here...
+bool ProcessSentence(std::wstring &sentence, SentenceInfo sentenceInfo) {
+  WSADATA wsaData;
+  return false;
+  int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (result != 0) {
+    return false;
+  }
 #ifdef COPY_CLIPBOARD
-	// This example extension automatically copies sentences from the hook currently selected by the user into the clipboard.
-	if (sentenceInfo["current select"])
-	{
-		HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, (sentence.size() + 2) * sizeof(wchar_t));
-		memcpy(GlobalLock(hMem), sentence.c_str(), (sentence.size() + 2) * sizeof(wchar_t));
-		GlobalUnlock(hMem);
-		OpenClipboard(0);
-		EmptyClipboard();
-		SetClipboardData(CF_UNICODETEXT, hMem);
-		CloseClipboard();
-	}
-	return false;
+  // This example extension automatically copies sentences from the hook
+  // currently selected by the user into the clipboard.
+  if (sentenceInfo["current select"]) {
+    HGLOBAL hMem =
+        GlobalAlloc(GMEM_MOVEABLE, (sentence.size() + 2) * sizeof(wchar_t));
+    memcpy(GlobalLock(hMem), sentence.c_str(),
+           (sentence.size() + 2) * sizeof(wchar_t));
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_UNICODETEXT, hMem);
+    CloseClipboard();
+  }
+  return false;
 #endif // COPY_CLIPBOARD
 
 #ifdef EXTRA_NEWLINES
-	// This example extension adds extra newlines to all sentences.
-	sentence += L"\r\n";
-	return true;
+  // This example extension adds extra newlines to all sentences.
+  sentence += L"\r\n";
+  return true;
 #endif // EXTRA_NEWLINES
 }
--- ExampleExtension/Extension.h
+++ ExampleExtension/Extension.h
@@ -1,26 +1,26 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 #include <cstdint>
 #include <string>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 
-
-struct InfoForExtension
-{
-	const char* name;
-	int64_t value;
+struct InfoForExtension {
+  const char *name;
+  int64_t value;
 };
 
-struct SentenceInfo
-{
-	const InfoForExtension* infoArray;
-	int64_t operator[](std::string propertyName)
-	{
-		for (auto info = infoArray; info->name; ++info) // nullptr name marks end of info array
-			if (propertyName == info->name) return info->value;
-		return *(int*)0xcccc = 0; // gives better error message than alternatives
-	}
+struct SentenceInfo {
+  const InfoForExtension *infoArray;
+  int64_t operator[](std::string propertyName) {
+    for (auto info = infoArray; info->name;
+         ++info) // nullptr name marks end of info array
+      if (propertyName == info->name)
+        return info->value;
+    return *(int *)0xcccc = 0; // gives better error message than alternatives
+  }
 };
 
 struct SKIP {};
