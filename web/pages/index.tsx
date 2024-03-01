fbe5c5db7e4cc5119d0d0b68708c07bcd402a9a8
OrkWard <orkward_o@outlook.com>
2024-03-01 22:00:56 +0800

    feat(web): improved web page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,34 +1,54 @@
 import { Inter } from "next/font/google";
+import { useEffect, useRef, useState } from "react";
 import useSWR from "swr";
-import type { Middleware } from "swr";
+import type { Middleware, SWRResponse } from "swr";
+import { z } from "zod";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type Sentences = {
+  sentence: string;
+  id: number;
+}[];
+
 const keepPrevious: Middleware = (useSWRNext) => {
   return (key, fetcher, config) => {
-    // hook 运行之前...
+    const savedSentences = useRef<(string[] | undefined)[]>([]);
 
-    // 处理下一个中间件，如果这是最后一个，则处理 `useSWR` hook。
     const swr = useSWRNext(key, fetcher, config);
 
-    // hook 运行之后...
-    return swr;
+    useEffect(() => {
+      const newSentences = (swr.data || []) as Sentences;
+      newSentences.forEach((v) => {
+        if (!savedSentences.current[v.id]) savedSentences.current[v.id] = [];
+        savedSentences.current[v.id]!.push(v.sentence);
+      });
+      console.count("middle");
+      console.log(newSentences);
+    }, [swr.data]);
+
+    return Object.assign({}, swr, { saved: savedSentences.current });
   };
 };
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR(
+  const { saved, error } = useSWR(
     "http://localhost:8081",
     async (url) => {
       const res = await fetch(url);
-      const sentences: string[] = await res.json();
+      console.count("fetch");
+      const sentences = z
+        .array(z.object({ sentence: z.string(), id: z.number() }))
+        .parse(await res.json());
       return sentences;
     },
     {
       refreshInterval: 500,
       use: [keepPrevious],
     }
-  );
+  ) as SWRResponse & { saved: string[][] };
+
+  const [textNumber, setTextNumber] = useState(1);
 
   return (
     <main
@@ -36,7 +56,22 @@ export default function Home() {
     >
       <div className="rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 w-full">
         <h2 className={`mb-3 text-2xl font-semibold`}>对话：</h2>
-        <p>{data}</p>
+        <div className="flex flex-row gap-1">
+          {Object.keys(saved).map((num) => (
+            <button
+              key={num}
+              onClick={() => {
+                setTextNumber(parseInt(num));
+              }}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+        {saved[textNumber]?.map((sentence, i) => (
+          <p key={i}>{sentence}</p>
+        ))}
       </div>
     </main>
   );
